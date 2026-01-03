19ddbeef8af45ebf00139d8fc61df46562c8f21f
Eliana <valdezeliana38@gmail.com>
2026-01-02 22:33:46 -0300

    how to use conditionals in py

---
+++ FASTAPI/fundamentos-python/03-conditionals.py
@@ -0,0 +1,13 @@
+is_new_dev = False
+knows_python = True
+
+# if True:
+#     print("Esto es verdadero")
+
+if is_new_dev:
+    print("bienvenido broder keloke")
+else:
+    print("ahre no tenia laburo")
+    
+if knows_python:
+    print("aprende fastapi bro")
