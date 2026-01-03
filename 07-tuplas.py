6b34d8fd57b1793b5ea039bc356a8752ceec8556
Eliana <valdezeliana38@gmail.com>
2026-01-03 19:44:26 -0300

    how to use tuple in python and its methods

---
+++ 07-tuplas.py
@@ -0,0 +1,15 @@
+my_tupla = (1, 2, 3, 4, "Hola", True, False, 2, "Hiii")
+
+# indexadas, permite duplicados, SON INMUTABLES, sus valores no cambian!
+
+print(my_tupla)
+print(my_tupla.count(2))
+print(my_tupla.index(2))
+
+# las tuplas son inmutables
+
+# my_tupla[1] = 20
+# print(my_tupla) #error!
+
+week = ("Lunes", "Martes", "Miercoles")
+# util para valores fijos
