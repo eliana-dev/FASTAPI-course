0f3db66f3b6d6d1356609d496cccef7044c185b1
Eliana <valdezeliana38@gmail.com>
2026-01-03 23:14:59 -0300

    use functions and keyword args

---
+++ FASTAPI/fundamentos-python/11-functions.py
@@ -0,0 +1,11 @@
+def hello(greet="Hii", name="Person"):
+    print(f"{greet}, {name}")
+
+
+hello("Holiwiiis", "elianita")
+hello("bye bye", "elianita")
+hello("안녕하세요", "엘리")
+hello("안녕히 가세요", "엘리")
+hello()  # defecto
+hello("pedro", "holii")
+hello(name="pedro", greet="holii") #    usa keyword args
