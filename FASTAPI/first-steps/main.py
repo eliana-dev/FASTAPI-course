b7a4e5cffe640dca2487290a12f3b25ec12b673e
Eliana <valdezeliana38@gmail.com>
2026-01-06 14:25:09 -0300

    add a route to get all the posts

--- FASTAPI/first-steps/main.py
+++ FASTAPI/first-steps/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Mini Blog")
 
+BLOG_POST = [
+    {"id": 1, "title": "Hola desde FastAPI", "Content": "Mi primer post con fastAPI"},
+    {"id": 2, "title": "Me gusta el chocolate", "Content": "Mi segundo post con fastAPI"},
+    {"id": 3, "title": "Holiwis yeii", "Content": "Mi tercer post con fastAPI"},
+]
 @app.get("/")
 def home():
     return {'message': 'Bienvenidos a mini blog!'}
 
+@app.get("/posts")
+def list_posts():
+    return {"data": BLOG_POST}
+
+
 #  fastapi dev main.py
 #evita usar run  
+#uv run uvicorn main:app --reload
