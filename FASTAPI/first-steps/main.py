497e56abc6ad831908b171ed9024436a32545113
Eliana <valdezeliana38@gmail.com>
2026-01-08 14:50:00 -0300

    how to use queryparams to filter

--- FASTAPI/first-steps/main.py
+++ FASTAPI/first-steps/main.py
@@ -1,21 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
 
 app = FastAPI(title="Mini Blog")
 
 BLOG_POST = [
-    {"id": 1, "title": "Hola desde FastAPI", "Content": "Mi primer post con fastAPI"},
-    {"id": 2, "title": "Me gusta el chocolate", "Content": "Mi segundo post con fastAPI"},
-    {"id": 3, "title": "Holiwis yeii", "Content": "Mi tercer post con fastAPI"},
+    {
+        "id": 1,
+        "title": "Hola desde FastAPI",
+        "Content": "Mi primer post con fastAPI",
+    },
+    {
+        "id": 2,
+        "title": "Me gusta el chocolate",
+        "Content": "Mi segundo post con fastAPI",
+    },
+    {
+        "id": 3,
+        "title": "Holiwis yeii",
+        "Content": "Mi tercer post con fastAPI",
+    },
 ]
+
+
 @app.get("/")
 def home():
-    return {'message': 'Bienvenidos a mini blog!'}
+    return {"message": "Bienvenidos a mini blog!"}
+
 
 @app.get("/posts")
-def list_posts():
+def list_posts(
+    query: Optional[str] = Query(  ##Optional[str] == str | None
+        default=None, description="texto para buscar por titulo"
+    ),
+):  # la query debe de ser de tipo str o none
+    if query:
+        results = [post for post in BLOG_POST if query.lower() in post["title"].lower()]
+        # for post in BLOG_POST:
+        #     if query.lower() in post["title"].lower():
+        #         results.append(post)
+        return {"data": results, "query": query}
     return {"data": BLOG_POST}
 
 
+# http://127.0.0.1:8000/posts?query=fastapi
+
+# .\.venv\Scripts\activate.ps1 -> activa el .venv
+# uv run uvicorn main:app --reload - inicia el servidor
 #  fastapi dev main.py
-#evita usar run  
-#uv run uvicorn main:app --reload
+# evita usar run
