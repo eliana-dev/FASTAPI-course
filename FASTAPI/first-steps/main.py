d0a03ee7904f62ea010331476757e2a82634a9c0
Eliana <valdezeliana38@gmail.com>
2026-01-17 16:11:28 -0300

    how to use path params + exercise of query params!

--- FASTAPI/first-steps/main.py
+++ FASTAPI/first-steps/main.py
@@ -29,20 +29,31 @@ def home():
 
 @app.get("/posts")
 def list_posts(
-    query: Optional[str] = Query(  
-        default=None, description="texto para buscar por titulo" ##Optional[str] == str | None
+    query: Optional[str] = Query(
+        default=None, description="texto para buscar por titulo"
     ),
-):  # la query debe de ser de tipo str o none
+):
     if query:
         results = [post for post in BLOG_POST if query.lower() in post["title"].lower()]
-        # for post in BLOG_POST:
-        #     if query.lower() in post["title"].lower():
-        #         results.append(post)
         return {"data": results, "query": query}
     return {"data": BLOG_POST}
 
 
-# http://127.0.0.1:8000/posts?query=fastapi
+@app.get("/posts/{post_id}")  ##query parameter: include_content=false
+def get_post(
+    post_id: int,
+    include_content: Optional[bool] = Query(
+        default=None, description="Bool para filtrar contenido"
+    ),
+):
+    for post in BLOG_POST:
+        if post["id"] == post_id:
+            if not include_content:
+                return {"id": post["id"], "title": post["title"]}
+            return {"data": post}
+
+    return {"error": "post no encontrado"}
+
 
 # .\.venv\Scripts\activate.ps1 -> activa el .venv
 # uv run uvicorn main:app --reload - inicia el servidor
